a3f469c8fbb17d1aaf49564af03a3c6c4ca0e72b
Yurii Dereguzov <DereguzovYE@yandex.ru>
2023-09-07 19:06:50 +0500

    feat: сделать бургер меню

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import logo from "../../images/logo.svg";
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function Header() {
   const location = useLocation();
   const path = location.pathname;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleOpenMenu() {
+    setIsOpen(!isOpen);
+  }
 
   return (
     <header className={path === "/" ? "header header_background" : "header"}>
@@ -11,27 +18,68 @@ function Header() {
         <Link to="/" className="header__logo">
           <img className="header__logo-img" src={logo} alt="Логотип" />
         </Link>
-        {path === "/movies" || path === "/saved-movies" || path === "/profile" ? (
+        {path === "/movies" ||
+        path === "/saved-movies" ||
+        path === "/profile" ? (
           <>
-            <nav className="header__navigation">
-              <Link to="/movies" className="navigation__link">
-                Фильмы
-              </Link>
-              <Link to="/saved-movies" className="navigation__link">
-                Сохранённые фильмы
-              </Link>
+            <nav
+              className={`header__navigation ${isOpen ? "menu_opened" : ""}`}
+            >
+              <ul className="navigation__container">
+                <li
+                  className={
+                    isOpen ? "navigation__link-item" : "display_inactive"
+                  }
+                >
+                  <Link to="/" className="navigation__link">
+                    Главная
+                  </Link>
+                </li>
+                <li
+                  className={`navigation__link-item ${
+                    path === "/movies" ? "link_active" : ""
+                  }`}
+                >
+                  <Link to="/movies" className="navigation__link">
+                    Фильмы
+                  </Link>
+                </li>
+                <li
+                  className={`navigation__link-item ${
+                    path === "/saved-movies" ? "link_active" : ""
+                  }`}
+                >
+                  <Link to="/saved-movies" className="navigation__link">
+                    Сохранённые фильмы
+                  </Link>
+                </li>
+              </ul>
             </nav>
             <Link
               to="/profile"
               className={
                 path === "/"
                   ? "header__navigation-account background_black"
-                  : "header__navigation-account background_white"
+                  : `header__navigation-account background_white ${
+                      isOpen ? "menu_opened" : ""
+                    }`
               }
             ></Link>
+            {!isOpen ? (
+              <div className="header__burger" onClick={handleOpenMenu}>
+                <div className="header__burger-line"></div>
+                <div className="header__burger-line"></div>
+                <div className="header__burger-line"></div>
+              </div>
+            ) : (
+              <button
+                className="header__burger-close"
+                onClick={handleOpenMenu}
+              ></button>
+            )}
           </>
         ) : (
-          <nav className="header__navigation">
+          <nav className="header__nav">
             <Link to="/sign-up" className="header__register-link">
               Регистрация
             </Link>
